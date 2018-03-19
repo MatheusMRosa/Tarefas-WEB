944f907535e02c059c23187e57a3c4a003ce6f71
Matheus Moises da Rosa <matheusmoises.rosa@gmail.com>
2018-03-19 12:46:17 -0300

    Adicionando Lista de Clientes

--- React/aula2/src/User.js
+++ React/aula2/src/User.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './User.css';
+import "./User.css";
 
 class User extends Component {
     constructor(props) {
@@ -7,12 +7,13 @@ class User extends Component {
         this.state = {
             nome: '',
             cpf: '',
-            loading: false
+            loading: false,
+            list: []
         };
         this.save = this.save.bind(this);
         this.onCPFChanged = this.onCPFChanged.bind(this);
         this.onNomeChanged = this.onNomeChanged.bind(this);
-        this.listClient = this.listClient(this);
+        this.listUser();
     }
 
     onCPFChanged(e) {
@@ -42,44 +43,76 @@ class User extends Component {
         return false;
     }
 
-    listClient() {
+    componentDidMount() {
+
+    }
+
+    listUser() {
         fetch('http://localhost:3001/user', {
             method: 'GET',
             headers: {
                 'Accept': 'application/json, text/plain, */*',
                 'Content-Type': 'application/json'
             }
-        });
+        }).then(res => res.json())
+            .then(data => {
+                this.setState({list: data});
+            });
+
         return false
-    }
-
-    componentDidMount() {
-
     }
 
     render() {
         return (
             <form onSubmit={this.save}>
-                <table align={"center"} className={'table'}>
+                <table align="center">
+                    <tbody>
                     <tr>
-                        <td>Nome</td>
-                        <td><input value={this.state.nome}
-                                   onChange={this.onNomeChanged}/></td>
+                        <td>
+                            <label>Nome</label>
+                        </td>
+                        <td>
+                            <input value={this.state.nome}
+                                   onChange={this.onNomeChanged}/>
+                        </td>
                     </tr>
                     <tr>
-                        <td>CPF</td>
-                        <td><input value={this.state.cpf}
-                                   onChange={this.onCPFChanged}/></td>
+                        <td>
+                            <label>CPF</label>
+                        </td>
+                        <td>
+                            <input value={this.state.cpf}
+                                   onChange={this.onCPFChanged}/>
+                        </td>
                     </tr>
+                    </tbody>
                 </table>
                 <button type="submit">Salvar</button>
-                <table align={"center"} className={'table'}>
-                    <th>
-                        <tr>
-                            <td>Nome</td>
-                            <td>CPF</td>
-                        </tr>
-                    </th>
+                <table align="center">
+                    <thead>
+                    <tr>
+                        <th>
+                            <label>Nome</label>
+                        </th>
+                        <th>
+                            <label>CPF</label>
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.list.map(item => {
+                        return (
+                            <tr key={item.name}>
+                                <td>
+                                    {item.nome}
+                                </td>
+                                <td>
+                                    {item.cpf}
+                                </td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
                 </table>
             </form>
         );
